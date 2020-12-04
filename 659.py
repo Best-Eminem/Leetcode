b31ee993521bb2869bd47b5cbd4468ddbc7b0d86
EminemBest <cyk_cd@163.com>
2020-12-04 18:09:06 +0800

    2020/12/4

--- 659.py
+++ 659.py
@@ -3,5 +3,15 @@
 # @Author  : Yike Cheng
 # @FileName: 659.py
 # @Software: PyCharm
-class Solution:
-    def isPossible(self, nums: List[int]) -> bool:
+import collections
+import heapq
+def isPossible(nums):
+    dic = collections.defaultdict(list)
+    for i in nums:
+        heap = dic.get(i - 1)
+        if heap:
+            preLength = heapq.heappop(heap)
+            heapq.heappush(dic[i], preLength + 1)
+        else:
+            heapq.heappush(dic[i], 1)
+    return not any(heap and heap[0] < 3 for heap in dic.values())
